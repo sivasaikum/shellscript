64df5547ebe860a49ea2a4382c791669bcec53d6
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-27 11:35:45 +0530

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Sai # no space before and after =
+PERSON2=Revanth
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hi $PERSON1"
+echo "$PERSON1 :: How are you doing ?"
+echo "$PERSON2 :: doing well, what about you ?"
+echo "$PERSON1 :: good, what's the plans for weekend"
+echo "$PERSON2 :: complete the pending series"
