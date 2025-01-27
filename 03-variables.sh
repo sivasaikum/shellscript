a960fa674cc41b8c9183fc0d76b30f12bec4015e
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-27 11:38:16 +0530

    conversation added

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@ PERSON1=Sai # no space before and after =
 PERSON2=Revanth
 
 echo "$PERSON1 :: Hi $PERSON2"
-echo "$PERSON2 :: Hi $PERSON1"
+echo "${PERSON2} :: Hi $PERSON1"
 echo "$PERSON1 :: How are you doing ?"
 echo "$PERSON2 :: doing well, what about you ?"
 echo "$PERSON1 :: good, what's the plans for weekend"
