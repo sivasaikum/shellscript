d94e14d2dce64f285cb198b3f175839443a8438e
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-27 11:30:38 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Sai :: Hi Revanth"
+echo "Revanth :: Hi Sai"
+echo "Sai :: How are you doing ?"
+echo "Revanth :: doing well, what about you ?"
+echo "Sai :: good, what's the plans for weekend"
+echo " Revanth :: complete the pending series"
