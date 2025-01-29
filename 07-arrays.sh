cad737be35cf1dea5d7557366ebc75868f5d0420
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-29 12:28:11 +0530

    ARRAYS

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,4 +3,11 @@
 MOVIES=("PUSHPA" "RRR" "DEVARA" "SALAR")
 # index starts from 0, size is 4
 
-echo "First Movie is : ${MOVIES[0]}"
+echo "First  Movie is : ${MOVIES[0]}"
+echo "Second Movie is : ${MOVIES[1]}"
+echo "Third  Movie is : ${MOVIES[2]}"
+echo "Fourth Movie is : ${MOVIES[3]}"
+
+#TO PRINT ALL VALUES OR VARIABLES
+
+echo "ALL Movie is : ${MOVIES[@]}"
