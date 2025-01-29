55f5f88697efa23cb7527ae8eb2cec9a3c9291ac
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-29 23:34:25 +0530

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if[ $NUMBER -gt 100]
+then
+    echo " the given number $NUMBER is : Greater than 100"
+else
+    echo " the given number $NUMBER is : Less than 100"
+fi
