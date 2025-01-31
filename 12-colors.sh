bc7861c558d83bbe9944c964ef52f9f7b821139c
sivasaikum <sivasaikumarallampati@gmail.com>
2025-01-31 12:05:28 +0530

    FUNCTIONS script

---
+++ 12-colors.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+USERID=$( id -u )
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE "
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: you must have a root access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "mysql installing"
+    
+else
+    echo -e "MYSQL is already ... $Y INSTALLED "
+fi
+
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Git installing"
+else 
+    echo -e "GIT is already ... $Y INSTALLED "
+fi
