a3c79cbdee5f9c3bf1406f3cc8d88a212bf4ae90
sivasaikum <sivasaikumarallampati@gmail.com>
2025-02-05 12:55:49 +0530

    loops script

---
+++ 15-loops.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+USERID=$( id -u )
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FLODER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FLODER/$LOG_FILE-$TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: you must have a root access to execute this script"
+    exit 1
+fi
+
+for package in $@
+do 
+    dnf list installed $package &>>$LOG_FILE_NAME
+    if [ $? -ne 0]
+    then 
+        dnf install $package -y &>>$LOG_FILE_NAME
+        $VALIDATE $? "installing $package"
+    else
+        echo -e " $package is already $G INSTALLED $N "
+    fi
+done
