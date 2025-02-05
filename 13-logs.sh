24fae49ebbd1a6a2ae3ed4630f26fd528966d0c4
sivasaikum <sivasaikumarallampati@gmail.com>
2025-02-05 12:07:08 +0530

    logs script

---
+++ 13-logs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+USERID=$( id -u )
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FLODER="var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME=$( $LOGS_FLODER/$LOG_FILE-$TIMESTAMP)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: you must have a root access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "mysql installing"
+    
+else
+    echo -e "MYSQL is already ... $G INSTALLED  "
+fi
+
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Git installing"
+else 
+    echo -e "GIT is already ... $G INSTALLED "
+fi
