5a3ed88d0094c73dd84c1971ae2c7d5d276d1bcf
sivasaikum <sivasaikumarallampati@gmail.com>
2025-02-13 11:58:21 +0530

    disk usage

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -8,7 +8,15 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITON=$(echo $line | awk -F " " '{print $NF}')
-    echo  $USAGE
+    #echo  $USAGE
+    if [ $USAGE -ge $DISK_THRESHOLD]
+    then 
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+    else
+        echo "DISK USAGE is good as f now"
+    fi 
 
 
 done <<< $DISK_USAGE
+
+echo -e "Message: $MSG"
