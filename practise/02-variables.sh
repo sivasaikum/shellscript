77dcace5b61c84cc116f03c418ee7174d1436fcf
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-26 07:51:26 +0530

    practice

---
+++ practise/02-variables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# PERSON1="SAI"
+# PERSON2="PAVAN"
+
+# echo " $PERSON1 :: Hi $PERSON2"
+# echo " $PERSON2 :: Hey Hi $PERSON1, How are you man?"
+# echo " $PERSON1 :: I am well and good, How are you $PERSON2"
+# echo " $PERSON2 :: I am also good. Whats the plan for today "
+
+
+
+# PERSON1=$1
+# PERSON2=$2
+
+# echo " $PERSON1 :: Hi $PERSON2"
+# echo " $PERSON2 :: Hey Hi $PERSON1, How are you man?"
+# echo " $PERSON1 :: I am well and good, How are you $PERSON2"
+# echo " $PERSON2 :: I am also good. Whats the plan for today "
+
+
+read -s USERNAME
+read -s PASSWORD
+
+echo "USERNAME IS :: ${USERNAME}"
+
+
