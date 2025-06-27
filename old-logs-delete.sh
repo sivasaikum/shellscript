dcc6d369ddb6e323f528e6b59475646ccb23effd
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-27 19:20:06 +0530

    practice

--- old-logs-delete.sh
+++ old-logs-delete.sh
@@ -2,6 +2,11 @@
 
 SOURCE_DIR="/home/ec2-user/app-logs"
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "Files going to delete are : 
-$FILES_TO_DELETE "
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)"
+
+while read -r deletelogs
+do
+    echo "Files going to delete are : 
+    $FILES_TO_DELETE "
+
+done <<< $FILES_TO_DELETE
