6092cffe475fcad7c9925aa812fd893eb4ba119e
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-28 16:57:23 +0530

    practice

--- backup.sh
+++ backup.sh
@@ -8,7 +8,6 @@ TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 
 
 echo "Script started at $TIMESTAMP "
-echo "$3"
 
 if [ $# -lt 2 ] ; then
     echo " Error , Usage is sh backup.sh <SOURCE_DIR> <DEST_DIR> "
@@ -25,7 +24,33 @@ if [ ! -d $DEST_DIR ] ; then
     exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$3)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files are :
 $FILES "
+
+if [ -n "$FILES" ] ; then
+    echo "Files are :
+    $FILES "
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    $FILES | zip @ "$ZIP_FILE"
+
+    if [ -f $ZIP_FILE ] ; then
+        echo "Files are succesfulled zipped , so deleteing the old log files"
+        
+        while read -r filelogs
+        do
+            rm-rf $filelogs
+            echo "deleting file logs are : $filelogs "
+
+        done <<< $FILES
+    else 
+        echo "Zip File is not created"
+    fi
+
+
+
+else
+    echo "There is no files to zip"
+fi
+
 
